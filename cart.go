9af483c256a1294604b1a6c9ac3c635813b458c2
cfmitrah <j_saravanamuthu@yahoo.com>
2023-05-11 20:24:57 +0530

    MW :: Added product details model in add-to-cart

--- cart.go
+++ cart.go
@@ -1,11 +1,21 @@
 package types
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/dialects/postgres"
+)
 
 // Cart req model
 type AddToCart struct {
 	gorm.Model
-	OrderCode string `json:"order_code" gorm:"type:varchar(50);column:order_code"`
-	Product   string `json:"product" gorm:"type:json;column:product"`
-	UserID    int    `json:"user_id" gorm:"type:int;column:user_id"`
+	OrderCode string         `json:"orderCode" gorm:"type:varchar(50);column:order_code"`
+	Product   postgres.Jsonb `json:"product" gorm:"type:json;column:product"`
+	UserID    int            `json:"userId" gorm:"type:int;column:user_id"`
+}
+
+type ProductDetails struct {
+	ProductID int    `json:"productId" validate:"required"`
+	Quantity  int    `json:"quantity" validate:"required"`
+	Price     int    `json:"price"`
+	ImageURL  string `json:"imageUrl" validate:"required"`
 }
