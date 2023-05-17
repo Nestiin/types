46f776504e0877ec86021d58b3521ad8d99fc738
cfmitrah <j_saravanamuthu@yahoo.com>
2023-05-17 11:20:21 +0530

    MW :: Modify the wish list model field.

--- wishlist.go
+++ wishlist.go
@@ -1,6 +1,8 @@
 package types
 
+import "github.com/jinzhu/gorm/dialects/postgres"
+
 type WishListReq struct {
-	UserID  int       `json:"user_id" validate:"required"`
-	Product []Product `json:"product" validate:"required"`
+	UserID  int            `json:"user_id" validate:"required"`
+	Product postgres.Jsonb `json:"product" validate:"required"`
 }
