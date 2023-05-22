d14e91f2139d082b14634d248a6a6cfe3f98e67c
cfmitrah <j_saravanamuthu@yahoo.com>
2023-05-22 19:23:29 +0530

    MW :: Added the updated by fields in the product models.

--- productType.go
+++ productType.go
@@ -9,6 +9,7 @@ type ProductType struct {
 	Description string `json:"description,omitempty" gorm:"type:varchar(5000);column:description"`
 	Status      string `json:"status,omitempty" gorm:"type:varchar(20);column:status"`
 	CreatedBy   string `json:"createdBy,omitempty" gorm:"type:varchar(50);column:created_by"`
+	UpdatedBy   string `json:"updatedBy,omitempty" gorm:"type:varchar(50);colum:updated_by"`
 }
 
 func (ProductType) TableName() string {
--- subcategory.go
+++ subcategory.go
@@ -10,6 +10,8 @@ type ProductSubCategory struct {
 	Status      string `json:"status,omitempty" gorm:"type:varchar(20);column:status"`
 	CategoryID  uint   `json:"categoryID,omitempty" gorm:"type:integer;column:category_id"`
 	CreatedBy   string `json:"createdBy,omitempty" gorm:"type:integer;column:created_by"`
+	UpdatedBy   string `json:"updatedBy,omitempty" gorm:"type:varchar(50);colum:updated_by"`
+
 }
 
 func (ProductSubCategory) TableName() string {
