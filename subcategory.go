41e8d0b589e519bc19173645e3bfb73a56044425
cfmitrah <j_saravanamuthu@yahoo.com>
2023-06-21 19:05:17 +0530

    MW :: Removed updatedBy from subCategory struct

--- subcategory.go
+++ subcategory.go
@@ -10,8 +10,6 @@ type ProductSubCategory struct {
 	Status      string `json:"status,omitempty" gorm:"type:varchar(20);column:status"`
 	CategoryID  uint   `json:"categoryID,omitempty" gorm:"type:integer;column:category_id"`
 	CreatedBy   string `json:"createdBy,omitempty" gorm:"type:integer;column:created_by"`
-	UpdatedBy   string `json:"updatedBy,omitempty" gorm:"type:varchar(50);column:updated_by"`
-
 }
 
 func (ProductSubCategory) TableName() string {
