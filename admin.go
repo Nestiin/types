b3667085c2552f421b358d4ca9c1f5accdf61952
cfmitrah <j_saravanamuthu@yahoo.com>
2023-06-22 15:17:43 +0530

    MW :: Updated the admin register struct

--- admin.go
+++ admin.go
@@ -4,23 +4,23 @@ import "github.com/jinzhu/gorm"
 
 type AdminRegister struct {
 	gorm.Model
-	AdminCode        string `json:"admin_code" gorm:"type:varchar(50);column:admin_code"`
-	FirstName        string `json:"first_name" gorm:"type:varchar(45);column:first_name"`
-	LastName         string `json:"last_name" gorm:"type:varchar(45);column:last_name"`
-	RoleID           uint   `json:"role_id" gorm:"type:int;column:role_id"`
-	UserName         string `json:"user_name" gorm:"type:varchar(45);column:user_name"`
+	Code             string `json:"code" gorm:"type:varchar(50);column:admin_code"`
+	FirstName        string `json:"firstName" gorm:"type:varchar(45);column:first_name"`
+	LastName         string `json:"lastName" gorm:"type:varchar(45);column:last_name"`
+	RoleID           uint   `json:"roleId" gorm:"type:int;column:role_id"`
+	UserName         string `json:"userName" gorm:"type:varchar(45);column:user_name"`
 	PhoneNo          string `json:"phone" gorm:"type:varchar(20);column:phone"`
 	Email            string `json:"email" gorm:"type:varchar(45);column:email"`
 	Password         string `json:"password" gorm:"type:varchar(45);column:password"`
-	IsFirstTimeLogin bool   `json:"is_first_time_login" gorm:"type:boolean;column:is_first_time_login"`
-	IsTokenVerified  bool   `json:"is_token_verified" gorm:"type:boolean;column:is_token_verified"`
-	IsActive         bool   `json:"is_active" gorm:"type:varchar;column:is_active"`
-	CreatePermission bool   `json:"create_permission" gorm:"-"`
-	UpdatePermission bool   `json:"update_permission" gorm:"-"`
-	ReadPermission   bool   `json:"read_permission" gorm:"-"`
-	DeletePermission bool   `json:"delete_permission" gorm:"-"`
-	CreatedBy        string `json:"created_by" gorm:"type:varchar(50);column:created_by"`
-	UpdatedBy        string `json:"updated_by" gorm:"type:varchar(50);column:updated_by"`
+	IsFirstTimeLogin bool   `json:"isFirstTimeLogin" gorm:"type:boolean;column:is_first_time_login"`
+	IsTokenVerified  bool   `json:"isTokenVerified" gorm:"type:boolean;column:is_token_verified"`
+	Status           string `json:"status" gorm:"type:varchar;column:status"`
+	CreatePermission bool   `json:"createPermission" gorm:"-"`
+	UpdatePermission bool   `json:"updatePermission" gorm:"-"`
+	ReadPermission   bool   `json:"readPermission" gorm:"-"`
+	DeletePermission bool   `json:"deletePermission" gorm:"-"`
+	CreatedBy        string `json:"createdBy" gorm:"type:varchar(50);column:created_by"`
+	UpdatedBy        string `json:"updatedBy" gorm:"type:varchar(50);column:updated_by"`
 }
 
 func (AdminRegister) TableName() string {
