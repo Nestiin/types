8c436c32bfd8f3b7a4939c62f58f4ebdfb64dbf3
cfmitrah <j_saravanamuthu@yahoo.com>
2023-07-05 15:40:05 +0530

    MW :: Commit for test purpose only

--- cart.go
+++ cart.go
@@ -2,15 +2,18 @@ package types
 
 import (
 	"github.com/jinzhu/gorm"
-	"github.com/jinzhu/gorm/dialects/postgres"
 )
 
 // Cart req model
 type AddToCart struct {
 	gorm.Model
-	OrderCode string         `json:"orderCode" gorm:"type:varchar(50);column:order_code"`
-	Product   postgres.Jsonb `json:"product" gorm:"type:json;column:product"`
-	UserID    int            `json:"userId" gorm:"type:int;column:user_id"`
+	OrderCode string `json:"orderCode" gorm:"type:varchar(50);column:order_code"`
+	ProductId int    `json:"productId" gorm:"type:int;column:product_id"`
+	// Product   postgres.Jsonb `json:"product" gorm:"type:json;column:product"`
+	UserID   int     `json:"userId" gorm:"type:int;column:user_id"`
+	Quantity int     `json:"quantity" gorm:"type:int;column:quantity"`
+	Price    int     `json:"price" gorm:"type:int;column:price"`
+	ImageURL []Image `json:"imageUrl" gorm:"-"`
 }
 
 func (AddToCart) TableName() string {
@@ -18,8 +21,7 @@ func (AddToCart) TableName() string {
 }
 
 type ProductDetails struct {
-	ProductID int    `json:"productId" validate:"required"`
-	Quantity  int    `json:"quantity" validate:"required"`
-	Price     int    `json:"price"`
-	ImageURL  string `json:"imageUrl" validate:"required"`
+	Quantity int     `json:"quantity" validate:"required"`
+	Price    int     `json:"price"`
+	ImageURL []Image `json:"imageUrl" validate:"required"`
 }
