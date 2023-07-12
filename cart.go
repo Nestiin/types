722aa5a425082218615b6bc41e14d4e6586f37d0
cfmitrah <j_saravanamuthu@yahoo.com>
2023-07-12 19:45:11 +0530

    MW :: Added variant id on cart table

--- cart.go
+++ cart.go
@@ -9,9 +9,8 @@ type AddToCart struct {
 	gorm.Model
 	OrderCode string `json:"orderCode" gorm:"type:varchar(50);column:order_code"`
 	ProductId int    `json:"productId" gorm:"type:int;column:product_id"`
-	// Product   postgres.Jsonb `json:"product" gorm:"type:json;column:product"`
 	UserID    int     `json:"userId" gorm:"type:int;column:user_id"`
-	VariantId int     `json:"variantId" gorm:"-"`
+	VariantId int     `json:"variantId" gorm:"type:int;column:variant_id"`
 	Quantity  int     `json:"quantity" gorm:"type:int;column:quantity"`
 	Price     int     `json:"price" gorm:"type:int;column:price"`
 	ImageURLs []Image `json:"imageUrl" gorm:"-"`
